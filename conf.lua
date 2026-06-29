cc4b7b20cdc81100f81b0dfd133c337c7107bfc4
v138468 <v138468@gmail.com>
2026-06-29 23:33:48 +0800

    init

---
+++ conf.lua
@@ -0,0 +1,10 @@
+function love.conf(t)
+    t.window.title = "LoveTower"
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.vsync = 1
+    t.console = false
+
+    t.modules.joystick = false
+    t.modules.physics = false
+end
