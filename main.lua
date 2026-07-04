0dd5a791eb177ef7a3a5e96dd2308e14057e7979
v138468 <v138468@gmail.com>
2026-07-04 19:20:44 +0800

    init

--- main.lua
+++ main.lua
@@ -1,38 +1,31 @@
----@type Game
-local Game = require "scripts.Game"
-require "scripts.managers.InputManager"
-require "scripts.managers.ScenceManager"
-require "scripts.spawner.EntitySpawner"
-require "scripts.managers.EntityManager"
-require "scripts.factory.EntityFactory"
-require "scripts.managers.StateManager"
-require "scripts.managers.UIManager"
-
+local https = require("https")
 function love.load()
-    love.graphics.setBackgroundColor(0.1, 0.1, 0.12)
-    local font = love.graphics.newFont("resource/fonts/chinese.ttf", 18)
-    love.graphics.setFont(font)
-    ScenceManager:load()
-    EntitySpawner:load()
-    EntityManager:load()
-    EntityFactory:load()
-    StateManager:load()
-    UIManager:load()
-    Game:load()
+    -- print("love.load")
+    print(https)
+    -- local url = "https://raw.githubusercontent.com/v012345/NightOwlToolsV2/refs/heads/main/README.md"
+
+    -- local body, statusCode, headers, statusText = http.request(url)
+
+    -- if statusCode == 200 then
+    --     love.filesystem.write("file.txt", body)
+    --     print("File downloaded successfully.")
+    -- else
+    --     print("Failed to download: " .. tostring(statusCode))
+    -- end
 end
 
-function love.update(dt)
-    Game:update(dt)
-end
+-- function love.update(dt)
+--     Game:update(dt)
+-- end
 
-function love.draw()
-    Game:draw()
-end
+-- function love.draw()
+--     Game:draw()
+-- end
 
-function love.keypressed(key)
-    InputManager:keypressed(key)
-end
+-- function love.keypressed(key)
+--     InputManager:keypressed(key)
+-- end
 
-function love.mousepressed(x, y, button)
-    InputManager:mousepressed(x, y, button)
-end
+-- function love.mousepressed(x, y, button)
+--     InputManager:mousepressed(x, y, button)
+-- end
