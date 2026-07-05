d50a860b7a49cc4544acbe70ffb1c6899960c4b4
v138468 <v138468@gmail.com>
2026-07-05 13:27:06 +0800

    init

---
+++ asset/scripts/Enum/UI.lua
@@ -0,0 +1,12 @@
+Enum = Enum or {}
+Enum.UI = {
+    T = 1,       --text
+    B = 2,       --box (can be rounded)
+    C = 3,       --column
+    R = 4,       --row
+    O = 5,       --object - must be a Node
+    ROOT = 7,
+    S = 8,       --slider
+    I = 9,       --input text box
+    padding = 0, --default padding
+}
