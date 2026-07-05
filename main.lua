757d56b7943e169c9e58bee90df61dce86eb84ad
v138468 <v138468@gmail.com>
2026-07-05 17:12:26 +0800

    init

--- main.lua
+++ main.lua
@@ -76,22 +76,6 @@ function love.draw()
     App.instance:draw()
 end
 
--- require "asset.scripts.hotfix.Hotfix"
--- conf.lua 中下载失败了 或 版本号小于旧的版本号, 这里也不做处理了, 直接进入游戏
--- if new_cfg and new_cfg.version > old_cfg.version then
---     -- 先下载 Hotfix.lua 文件
---     local status_code, body = https.request(
---         "https://raw.githubusercontent.com/v012345/LoveTower/refs/heads/main/asset/scripts/hotfix/Hotfix.lua")
---     if status_code == 200 then
---         love.filesystem.createDirectory("asset/scripts/hotfix")
---         love.filesystem.write("asset/scripts/hotfix/Hotfix.lua", body)
---     end
---     require "asset.scripts.hotfix.Hotfix"
--- else
---     require "asset.scripts.app.Game"
--- end
-
-
 function love.resize(w, h)
     if w / h < 1 then -- Dont allow the screen to be too square, since pop in occurs above and below screen
         h = w / 1
