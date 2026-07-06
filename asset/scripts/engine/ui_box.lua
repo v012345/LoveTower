fc5a36bf2fdeb434e5b5fd90ea0fc46beaa07970
v138468 <v138468@gmail.com>
2026-07-06 13:17:41 +0800

    init

--- asset/scripts/engine/ui_box.lua
+++ asset/scripts/engine/ui_box.lua
@@ -29,9 +29,19 @@ end
 
 function UIBox:draw()
     print('UIBox:draw')
-    love.graphics.setColor(1, 1, 0.5)
-    love.graphics.circle("fill", 0, 0, 100)
-    love.graphics.setColor(1, 1, 1)
+    local realw, realh = love.window.getMode()
+    -- love.graphics.setCanvas()
+    -- love.graphics.push()
+    -- love.graphics.setShader()
+    love.graphics.clear(0, 0, 0, 1)
+    love.graphics.setColor(0.6, 0.8, 0.9, 1)
+    -- if progress > 0 then love.graphics.rectangle('fill', realw / 2 - 150, realh / 2 - 15, progress * 300, 30, 5) end
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.setLineWidth(3)
+    love.graphics.rectangle('line', realw / 2 - 150, realh / 2 - 15, 300, 30, 5)
+    love.graphics.print("LOADING: ", realw / 2 - 150, realh / 2 + 40)
+    -- love.graphics.pop()
+    -- love.graphics.present()
 end
 
 function UIBox:recalculate()
