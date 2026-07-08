2f04a4c548de3baab744f394fd6733856e17ac1b
v138468 <v138468@gmail.com>
2026-07-08 13:01:40 +0800

    init

--- asset/scripts/engine/timer.lua
+++ asset/scripts/engine/timer.lua
@@ -1,4 +1,9 @@
 ---@class Timer:Object
+---@field TOTAL number 累计时间, 受 SPEEDFACTOR 影响
+---@field REAL number 和 UPTIME 一样, 但是会被手动修改
+---@field REAL_SHADER number
+---@field UPTIME number 就是游戏第一次 update 到现在的真实累计时间
+---@field BACKGROUND number
 Timer = Object:extend()
 function Timer:init()
     self.TOTAL = 0
@@ -6,10 +11,11 @@ function Timer:init()
     self.REAL_SHADER = 0
     self.UPTIME = 0
     self.BACKGROUND = 0
+    self.SPEEDFACTOR = 1
 end
 
 function Timer:update(dt)
-    self.TOTAL = self.TOTAL + dt
+    self.TOTAL = self.TOTAL + dt * self.SPEEDFACTOR
     self.REAL = self.REAL + dt
     self.REAL_SHADER = self.REAL_SHADER + dt
     self.UPTIME = self.UPTIME + dt
