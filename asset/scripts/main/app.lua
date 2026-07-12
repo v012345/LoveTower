3a876929c8f36be75f370f24f4c11d3df4af20c0
v138468 <v138468@gmail.com>
2026-07-12 19:56:13 +0800

    init

--- asset/scripts/main/app.lua
+++ asset/scripts/main/app.lua
@@ -154,7 +154,8 @@ function App:main_menu()
     self:prep_stage(STAGES.MAIN_MENU, STATES.MENU, true)
 
     --- 创建主菜单场景
-    local img = Sprite(-30, -13, G.ROOM.T.w + 60, G.ROOM.T.h + 22, G.ASSET_ATLAS["ui_1"], { x = 2, y = 0 })
+    local room = Room.instance:get_root_node()
+    local img = Sprite(Transform(-30, -13, room.T.w + 60, room.T.h + 22), {}, { x = 2, y = 0 })
     --- 创建主菜单场景
 end
 
