4150170d4f1454a99eaff222a6f46a04749d8993
v138468 <v138468@gmail.com>
2026-07-12 20:14:13 +0800

    init

--- asset/scripts/engine/config.lua
+++ asset/scripts/engine/config.lua
@@ -1,4 +1,13 @@
----@class Config
-Config = {
+---@class Config: Object
+Config = Object:extend()
 
-}
+function Config:init()
+    self.asset_atli = require "asset.scripts.config.asset_atli"
+end
+
+function Config:get_asset_atli()
+    return self.asset_atli
+end
+
+---@type Config
+Config.instance = Config()
