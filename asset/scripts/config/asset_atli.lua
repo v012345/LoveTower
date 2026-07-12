4fc7e998dc0e874a96c624b7e3a47fdbc5a2a771
v138468 <v138468@gmail.com>
2026-07-12 20:37:22 +0800

    init

--- asset/scripts/config/asset_atli.lua
+++ asset/scripts/config/asset_atli.lua
@@ -1,383 +1,383 @@
 return {
     {
         name = "cards_1",
-        path = "resources/textures/1x/8BitDeck.png",
+        path = "asset/resources/textures/1x/8BitDeck.png",
         px = 71,
         py = 95
     },
     {
         name = "cards_2",
-        path = "resources/textures/1x/8BitDeck_opt2.png",
+        path = "asset/resources/textures/1x/8BitDeck_opt2.png",
         px = 71,
         py = 95
     },
     {
         name = "centers",
-        path = "resources/textures/1x/Enhancers.png",
+        path = "asset/resources/textures/1x/Enhancers.png",
         px = 71,
         py = 95
     },
     {
         name = "Joker",
-        path = "resources/textures/1x/Jokers.png",
+        path = "asset/resources/textures/1x/Jokers.png",
         px = 71,
         py = 95
     },
     {
         name = "Tarot",
-        path = "resources/textures/1x/Tarots.png",
+        path = "asset/resources/textures/1x/Tarots.png",
         px = 71,
         py = 95
     },
     {
         name = "Voucher",
-        path = "resources/textures/1x/Vouchers.png",
+        path = "asset/resources/textures/1x/Vouchers.png",
         px = 71,
         py = 95
     },
     {
         name = "Booster",
-        path = "resources/textures/1x/boosters.png",
+        path = "asset/resources/textures/1x/boosters.png",
         px = 71,
         py = 95
     },
     {
         name = "ui_1",
-        path = "resources/textures/1x/ui_assets.png",
+        path = "asset/resources/textures/1x/ui_assets.png",
         px = 18,
         py = 18
     },
     {
         name = "ui_2",
-        path = "resources/textures/1x/ui_assets_opt2.png",
+        path = "asset/resources/textures/1x/ui_assets_opt2.png",
         px = 18,
         py = 18
     },
     {
         name = "balatro",
-        path = "resources/textures/1x/balatro.png",
+        path = "asset/resources/textures/1x/balatro.png",
         px = 333,
         py = 216
     },
     {
         name = 'gamepad_ui',
-        path = "resources/textures/1x/gamepad_ui.png",
+        path = "asset/resources/textures/1x/gamepad_ui.png",
         px = 32,
         py = 32
     },
     {
         name = 'icons',
-        path = "resources/textures/1x/icons.png",
+        path = "asset/resources/textures/1x/icons.png",
         px = 66,
         py = 66
     },
     {
         name = 'tags',
-        path = "resources/textures/1x/tags.png",
+        path = "asset/resources/textures/1x/tags.png",
         px = 34,
         py = 34
     },
     {
         name = 'stickers',
-        path = "resources/textures/1x/stickers.png",
+        path = "asset/resources/textures/1x/stickers.png",
         px = 71,
         py = 95
     },
     {
         name = 'chips',
-        path = "resources/textures/1x/chips.png",
+        path = "asset/resources/textures/1x/chips.png",
         px = 29,
         py = 29
     },
 
     {
         name = 'collab_AU_1',
-        path = "resources/textures/1x/collabs/collab_AU_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_AU_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_AU_2',
-        path = "resources/textures/1x/collabs/collab_AU_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_AU_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_TW_1',
-        path = "resources/textures/1x/collabs/collab_TW_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_TW_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_TW_2',
-        path = "resources/textures/1x/collabs/collab_TW_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_TW_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_VS_1',
-        path = "resources/textures/1x/collabs/collab_VS_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_VS_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_VS_2',
-        path = "resources/textures/1x/collabs/collab_VS_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_VS_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_DTD_1',
-        path = "resources/textures/1x/collabs/collab_DTD_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_DTD_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_DTD_2',
-        path = "resources/textures/1x/collabs/collab_DTD_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_DTD_2.png",
         px = 71,
         py = 95
     },
 
     {
         name = 'collab_CYP_1',
-        path = "resources/textures/1x/collabs/collab_CYP_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_CYP_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_CYP_2',
-        path = "resources/textures/1x/collabs/collab_CYP_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_CYP_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_STS_1',
-        path = "resources/textures/1x/collabs/collab_STS_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_STS_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_STS_2',
-        path = "resources/textures/1x/collabs/collab_STS_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_STS_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_TBoI_1',
-        path = "resources/textures/1x/collabs/collab_TBoI_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_TBoI_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_TBoI_2',
-        path = "resources/textures/1x/collabs/collab_TBoI_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_TBoI_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_SV_1',
-        path = "resources/textures/1x/collabs/collab_SV_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_SV_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_SV_2',
-        path = "resources/textures/1x/collabs/collab_SV_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_SV_2.png",
         px = 71,
         py = 95
     },
 
     {
         name = 'collab_SK_1',
-        path = "resources/textures/1x/collabs/collab_SK_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_SK_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_SK_2',
-        path = "resources/textures/1x/collabs/collab_SK_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_SK_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_DS_1',
-        path = "resources/textures/1x/collabs/collab_DS_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_DS_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_DS_2',
-        path = "resources/textures/1x/collabs/collab_DS_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_DS_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_CL_1',
-        path = "resources/textures/1x/collabs/collab_CL_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_CL_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_CL_2',
-        path = "resources/textures/1x/collabs/collab_CL_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_CL_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_D2_1',
-        path = "resources/textures/1x/collabs/collab_D2_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_D2_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_D2_2',
-        path = "resources/textures/1x/collabs/collab_D2_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_D2_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_PC_1',
-        path = "resources/textures/1x/collabs/collab_PC_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_PC_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_PC_2',
-        path = "resources/textures/1x/collabs/collab_PC_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_PC_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_WF_1',
-        path = "resources/textures/1x/collabs/collab_WF_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_WF_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_WF_2',
-        path = "resources/textures/1x/collabs/collab_WF_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_WF_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_EG_1',
-        path = "resources/textures/1x/collabs/collab_EG_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_EG_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_EG_2',
-        path = "resources/textures/1x/collabs/collab_EG_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_EG_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_XR_1',
-        path = "resources/textures/1x/collabs/collab_XR_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_XR_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_XR_2',
-        path = "resources/textures/1x/collabs/collab_XR_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_XR_2.png",
         px = 71,
         py = 95
     },
 
     {
         name = 'collab_CR_1',
-        path = "resources/textures/1x/collabs/collab_CR_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_CR_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_CR_2',
-        path = "resources/textures/1x/collabs/collab_CR_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_CR_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_BUG_1',
-        path = "resources/textures/1x/collabs/collab_BUG_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_BUG_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_BUG_2',
-        path = "resources/textures/1x/collabs/collab_BUG_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_BUG_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_FO_1',
-        path = "resources/textures/1x/collabs/collab_FO_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_FO_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_FO_2',
-        path = "resources/textures/1x/collabs/collab_FO_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_FO_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_DBD_1',
-        path = "resources/textures/1x/collabs/collab_DBD_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_DBD_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_DBD_2',
-        path = "resources/textures/1x/collabs/collab_DBD_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_DBD_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_C7_1',
-        path = "resources/textures/1x/collabs/collab_C7_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_C7_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_C7_2',
-        path = "resources/textures/1x/collabs/collab_C7_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_C7_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_R_1',
-        path = "resources/textures/1x/collabs/collab_R_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_R_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_R_2',
-        path = "resources/textures/1x/collabs/collab_R_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_R_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_AC_1',
-        path = "resources/textures/1x/collabs/collab_AC_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_AC_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_AC_2',
-        path = "resources/textures/1x/collabs/collab_AC_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_AC_2.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_STP_1',
-        path = "resources/textures/1x/collabs/collab_STP_1.png",
+        path = "asset/resources/textures/1x/collabs/collab_STP_1.png",
         px = 71,
         py = 95
     },
     {
         name = 'collab_STP_2',
-        path = "resources/textures/1x/collabs/collab_STP_2.png",
+        path = "asset/resources/textures/1x/collabs/collab_STP_2.png",
         px = 71,
         py = 95
     }
