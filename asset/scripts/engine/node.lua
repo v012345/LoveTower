fa980149dda8b2bb450e064bd722dfdb42c80204
v138468 <v138468@gmail.com>
2026-07-13 20:35:33 +0800

    init

--- asset/scripts/engine/node.lua
+++ asset/scripts/engine/node.lua
@@ -195,7 +195,10 @@ function Node:set_container(container)
     self.container = container
 end
 
---Translation function used before any draw calls, translates this node according to the transform of the container node
+---目前不知道进来的时候原点在什么地方, 但是根据这个函数来看, 进来的时候原点应该在 container 的左上角
+---进入之后, 先移动到 container 的中心, 然后旋转, 然后移动到 container 的左上角
+---再移动到 x , y , 这里我没有看明白
+---Translation function used before any draw calls, translates this node according to the transform of the container node
 function Node:translate_container()
     if self.container and self.container ~= self then
         local unit_tile = Tile.instance:get_pixels_per_tile()
