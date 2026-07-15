06c89e9d82e55023e7a5582d42e7f1b46f299e1d
v138468 <v138468@gmail.com>
2026-07-15 08:50:30 +0800

    init

---
+++ api/vec2_api.lua
@@ -0,0 +1,10 @@
+---@meta
+
+---@class Vec2
+---@field x number
+---@field y number
+
+---@param x? number
+---@param y? number
+---@return Vec2
+function Vec2(x, y) end
---
+++ asset/scripts/engine/vec2.lua
@@ -0,0 +1,11 @@
+---@class Vec2
+Vec2 = Object:extend()
+
+function Vec2:init(x, y)
+    self.x = x or 0
+    self.y = y or 0
+end
+
+function Vec2:clone()
+    return Vec2(self.x, self.y)
+end
