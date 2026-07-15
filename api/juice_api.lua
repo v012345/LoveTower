9fcfb2281e563c268a71e70e689449d14f9e47a9
v138468 <v138468@gmail.com>
2026-07-16 00:45:12 +0800

    init

--- api/juice_api.lua
+++ api/juice_api.lua
@@ -1,8 +1,21 @@
+---@meta
+
 ---@class Juice:Object
 ---@field scale number
 ---@field scale_amt number
----@field r number
----@field r_amt number
+---@field r number rotation
+---@field r_amt number rotation_amt
 ---@field start_time number
 ---@field end_time number
 ---@field handled_elsewhere boolean
+
+
+---@param scale number
+---@param scale_amt number
+---@param rotation number
+---@param rotation_amt number
+---@param start_time number
+---@param end_time number
+---@param handled_elsewhere? boolean
+---@return Juice
+function Juice(scale, scale_amt, rotation, rotation_amt, start_time, end_time, handled_elsewhere) end
--- asset/scripts/engine/juice.lua
+++ asset/scripts/engine/juice.lua
@@ -2,3 +2,12 @@
 
 ---@class Juice
 Juice = Object:extend()
+
+function Juice:init(amount, rot_amt)
+    self.scale = 0
+    self.scale_amt = amount
+    self.r = 0
+    self.r_amt = rot_amt
+    self.start_time = 0
+    self.end_time = self.start_time + 0.4
+end
