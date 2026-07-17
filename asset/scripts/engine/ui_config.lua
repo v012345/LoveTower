b38f3db0b4319943d1548e5b25e5c44b31720f6e
v138468 <v138468@gmail.com>
2026-07-17 19:58:52 +0800

    init

---
+++ asset/scripts/engine/ui_config.lua
@@ -0,0 +1,13 @@
+---@class UIConfig
+UIConfig = Object:extend()
+
+function UIConfig:init()
+    self.object = nil
+    self.text = nil
+    self.scale = nil
+    self.colour = nil
+    self.align = nil
+    self.offset = nil
+    self.major = nil
+    self.bond = nil
+end
