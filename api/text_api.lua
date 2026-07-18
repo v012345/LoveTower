cdcf15cb7cf9922996bae406b7f30647e909d278
v138468 <v138468@gmail.com>
2026-07-18 11:41:14 +0800

    init

--- api/text_api.lua
+++ api/text_api.lua
@@ -1,1 +1,27 @@
 ---@class DynaText : Moveable
+
+
+---@class DynaTextConfig
+---@field scale number
+---@field string string
+---@field maxw number
+---@field colours Color[]
+---@field float boolean
+---@field shadow boolean
+---@field silent boolean
+---@field pop_in number
+---@field pop_in_rate number
+---@field bump_rate number
+---@field bump_amount number
+---@field font FontConfig
+
+
+
+---@class FontConfig
+---@field file string
+---@field render_scale number
+---@field TEXT_HEIGHT_SCALE number
+---@field TEXT_OFFSET Vec2
+---@field FONTSCALE number
+---@field squish number
+---@field DESCSCALE number
