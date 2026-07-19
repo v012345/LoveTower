92d00746186d16abd3f129cbd0c1671c61b9a807
v138468 <v138468@gmail.com>
2026-07-19 12:23:56 +0800

    init

--- api/text_api.lua
+++ api/text_api.lua
@@ -1,6 +1,6 @@
 ---@class (partial) DynaText:Moveable
 ---@field config DynaTextConfig
----@field shadow boolean
+---@field shadow boolean 是否显示阴影
 ---@field scale number
 ---@field pop_in_rate number
 ---@field bump_rate number
@@ -19,7 +19,7 @@
 ---@field maxw number
 ---@field colours Color[]
 ---@field float boolean
----@field shadow boolean
+---@field shadow boolean 是否显示阴影
 ---@field silent boolean
 ---@field pop_in number
 ---@field pop_in_rate number
@@ -36,13 +36,14 @@
 
 
 ---@class DynaTextConfigString
----@field ref_table table
----@field ref_value string
+---@field ref_table table 引用表
+---@field ref_value string 引用值
 ---@field prefix string
 ---@field suffix string
 ---@field scale number
 ---@field outer_colour Color
 ---@field colour Color
+---@field string string 如果 ref_table[ref_value] 不存在, 则使用此 string
 
 
 ---@class FontConfig
