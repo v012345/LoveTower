06a4a4cec95cb11a30013fa961f5df84a2913857
v138468 <v138468@gmail.com>
2026-07-31 13:04:43 +0800

    init

--- asset/scripts/engine/text.lua
+++ asset/scripts/engine/text.lua
@@ -8,6 +8,8 @@ function DynaText:init(config)
     Moveable.init(self, Transform(), Room.instance:get_root_node())
     config = config or {}
     self.config = TextConfig(config)
+    self.T.w = self.config.W
+    self.T.h = self.config.H
     self.text_offset = {
         x = self.font.TEXT_OFFSET.x * self.config.scale + (self.config.x_offset or 0),
         y = self.font.TEXT_OFFSET.y * self.config.scale + (self.config.y_offset or 0),
@@ -45,122 +47,6 @@ end
 function DynaText:update(dt)
     self:update_text()
     self:align_letters()
-end
-
-
---- update_text 的 first_pass 为 true 使用这个函数来初始化字符串, 目的是分享 update_text 的耦合关系
----@param config DynaTextConfig
----@return DynaTextConfig
-function DynaText:parse_config(config)
-    self.strings = {}
-    self.config = config
-    self.config.spacing = self.config.spacing or 0
-    self.pop_in_rate = config.pop_in_rate or 3
-    self.bump_rate = config.bump_rate or 2.666
-    self.bump_amount = config.bump_amount or 1
-    self.font = config.font or Language.instance.LANG.font
-    config.string = config.string or { 'HELLO WORLD' }
-    self.string = config.string
-    self.config.W = 0
-    self.config.H = 0
-    self.colours = config.colours or { Color.RED }
-    self.silent = (config.silent)
-    for k, v in ipairs(self.config.string) do
-        local part_a = 0       -- 前缀的索引
-        local part_b = 1000000 -- 后缀的索引
-        local new_string = nil
-        local outer_colour = nil
-        local inner_colour = nil
-        local part_scale = 1 -- 此字符串的自己的缩放比例, 来自己 DynaTextConfigString 的 scale 属性
-        -- 如果 v 是一个表, 就把 v 解出来, 转为一个字符串
-        if type(v) == 'table' and (v.ref_table or v.string) then
-            new_string = v.prefix .. tostring(v.ref_table and v.ref_table[v.ref_value] or v.string) .. v.suffix
-            part_a = #v.prefix
-            part_b = #new_string - #v.suffix
-            part_scale = v.scale
-            outer_colour = v.outer_colour or nil
-            inner_colour = v.colour or nil
-        else
-            new_string = v --[[@as string]]
-        end
-
-        self.strings[k] = {
-            string = new_string,
-            letters = {},
-            W = 0,
-            H = 0,
-            W_offset = 0,
-            H_offset = 0,
-        }
-        self.strings[k].string = new_string
-        local tempW = 0
-        local tempH = 0
-        local current_letter = 1 -- 当前字符的索引
-        local font_scale = self.font.FONTSCALE
-
-        for _, c in utf8.chars(new_string) do
-            local letters = {}
-            ---@type DynaTextLetter
-            local let_tab = {
-                letter = love.graphics.newText(self.font.FONT, c),
-                char = c,
-                scale = part_scale,
-                r = 0,
-                offset = { x = 0, y = 0 },
-                dims = {
-                    x = (self.font.FONT:getWidth(c) * part_scale + 2.7 * self.config.spacing) * self.config.scale,
-                    y = self.font.FONT:getHeight() * part_scale * self.font.TEXT_HEIGHT_SCALE * self.config.scale
-                },
-                pop_in = 1,
-                prefix = current_letter <= part_a and outer_colour or nil,
-                suffix = current_letter > part_b and outer_colour or nil,
-                inner_colour or nil
-            }
-
-            tempW = tempW + let_tab.dims.x * font_scale / Tile.instance.TILESIZE
-            tempH = math.max(let_tab.dims.y * font_scale / Tile.instance.TILESIZE, tempH)
-            letters[current_letter] = let_tab
-            self.strings[k].letters = letters
-            current_letter = current_letter + 1
-        end
-
-
-
-
-
-
-        self.strings[k].W = tempW
-        self.strings[k].H = tempH
-        -- self.config.W 和 self.config.H 是所有字符串中最大的宽度和高度
-
-        if self.strings[k].W > self.config.W then
-            self.config.W = self.strings[k].W
-        end
-        if self.strings[k].H > self.config.H then
-            self.config.H = self.strings[k].H
-        end
-    end
-
-
-    if self.config.maxw and self.config.W > self.config.maxw then
-        local old_scale = self.config.scale
-        local new_scale = self.config.scale * (self.config.maxw / self.config.W)
-        for k, v in ipairs(self.strings) do
-            for _, letter in ipairs(v.letters) do
-                letter.dims.x = letter.dims.x * new_scale / old_scale
-            end
-        end
-        self.config.scale = new_scale
-    end
-
-    self.T.w = self.config.W
-    self.T.h = self.config.H
-
-    for k, v in ipairs(self.strings) do
-        v.W_offset = 0.5 * (self.config.W - v.W)
-        v.H_offset = 0.5 * (self.config.H - v.H + (self.config.offset_y or 0))
-    end
-    return self.config
 end
 
 function DynaText:update_text(first_pass)
