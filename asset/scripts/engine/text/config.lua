75ef0256ceb13ada23e09ed768a557b25e1bc301
v138468 <v138468@gmail.com>
2026-08-01 16:35:42 +0800

    init

--- asset/scripts/engine/text/config.lua
+++ asset/scripts/engine/text/config.lua
@@ -1,8 +1,59 @@
 ---@class (partial) DynaTextConfig: Object
 DynaTextConfig = Object:extend()
 
+---@param data DynaTextConfigData
+function DynaTextConfig:init(data)
+    self.data = data
+    self.string_configs = {}
+    data.spacing = data.spacing or 0
+    data.pop_in_rate = data.pop_in_rate or 3
+    data.bump_rate = data.bump_rate or 2.666
+    data.bump_amount = data.bump_amount or 1
+    data.font = data.font or Language.instance.LANG.font
+    data.string_config_datas = data.string_config_datas or {}
+    data.colours = data.colours or { Color.RED }
+    data.silent = data.silent or false
+    data.W = 0
+    data.H = 0
+    for k, v in ipairs(data.string_config_datas) do
+        self.string_configs[k] = StringConfig(v)
+    end
+
+
+    if self.data.maxw and self.data.W > self.data.maxw then
+        local old_scale = self.data.scale
+        local new_scale = self.data.scale * (self.data.maxw / self.data.W)
+        for k, v in ipairs(self.strings) do
+            for _, letter in ipairs(v.letters) do
+                letter.dims.x = letter.dims.x * new_scale / old_scale
+            end
+        end
+        self.data.scale = new_scale
+    end
+
+
+
+    for k, v in ipairs(self.strings) do
+        v.W_offset = 0.5 * (self.data.W - v.W)
+        v.H_offset = 0.5 * (self.data.H - v.H + (self.data.offset_y or 0))
+    end
+    return self.data
+end
+
 ---@class (partial) StringConfig: Object
 StringConfig = Object:extend()
+
+---@param config StringConfigData
+function StringConfig:init(config)
+    self.data = config
+    config.prefix = config.prefix or ''
+    config.suffix = config.suffix or ''
+    config.scale = config.scale or 1
+    config.outer_colour = config.outer_colour or nil
+    config.colour = config.colour or nil
+    config.ref_table = config.ref_table or {}
+    config.ref_value = config.ref_value or nil
+end
 
 ---@class (partial) LetterConfig: Object
 LetterConfig = Object:extend()
@@ -22,55 +73,4 @@ function LetterConfig:__tostring()
     scale: %s
     colour: %s
     offset: %s]], self.data.char, self.data.scale, self.data.colour, self.offset)
-end
-
----@param config StringConfigData
-function StringConfig:init(config)
-    self.data = config
-    config.prefix = config.prefix or ''
-    config.suffix = config.suffix or ''
-    config.scale = config.scale or 1
-    config.outer_colour = config.outer_colour or nil
-    config.colour = config.colour or nil
-    config.ref_table = config.ref_table or {}
-    config.ref_value = config.ref_value or nil
-end
-
----@param config DynaTextConfigData
-function DynaTextConfig:init(config)
-    self.data = config
-    self.string_configs = {}
-    config.spacing = config.spacing or 0
-    config.pop_in_rate = config.pop_in_rate or 3
-    config.bump_rate = config.bump_rate or 2.666
-    config.bump_amount = config.bump_amount or 1
-    config.font = config.font or Language.instance.LANG.font
-    config.strings = config.strings or {}
-    config.colours = config.colours or { Color.RED }
-    config.silent = config.silent or false
-    config.W = 0
-    config.H = 0
-    for k, v in ipairs(config.string_config_datas) do
-      self.string_configs[k] = StringConfig(v)
-    end
-
-
-    if self.config.maxw and self.config.W > self.config.maxw then
-        local old_scale = self.config.scale
-        local new_scale = self.config.scale * (self.config.maxw / self.config.W)
-        for k, v in ipairs(self.strings) do
-            for _, letter in ipairs(v.letters) do
-                letter.dims.x = letter.dims.x * new_scale / old_scale
-            end
-        end
-        self.config.scale = new_scale
-    end
-
-
-
-    for k, v in ipairs(self.strings) do
-        v.W_offset = 0.5 * (self.config.W - v.W)
-        v.H_offset = 0.5 * (self.config.H - v.H + (self.config.offset_y or 0))
-    end
-    return self.config
 end
