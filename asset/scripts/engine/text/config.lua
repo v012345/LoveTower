2b9909e80e8340d386853213a0ae84d4d225190d
v138468 <v138468@gmail.com>
2026-08-01 19:22:54 +0800

    init

--- asset/scripts/engine/text/config.lua
+++ asset/scripts/engine/text/config.lua
@@ -5,37 +5,8 @@ DynaTextConfig = Object:extend()
 function DynaTextConfig:init(data)
     self.data = data
     self.string_configs = {}
-    data.spacing = data.spacing or 0
-    data.pop_in_rate = data.pop_in_rate or 3
-    data.bump_rate = data.bump_rate or 2.666
-    data.bump_amount = data.bump_amount or 1
-    data.font = data.font or Language.instance.LANG.font
-    data.string_config_datas = data.string_config_datas or {}
-    data.colours = data.colours or { Color.RED }
-    data.silent = data.silent or false
-    data.W = 0
-    data.H = 0
     for k, v in ipairs(data.string_config_datas) do
         self.string_configs[k] = StringConfig(v)
-    end
-
-
-    if self.data.maxw and self.data.W > self.data.maxw then
-        local old_scale = self.data.scale
-        local new_scale = self.data.scale * (self.data.maxw / self.data.W)
-        for k, v in ipairs(self.strings) do
-            for _, letter in ipairs(v.letters) do
-                letter.dims.x = letter.dims.x * new_scale / old_scale
-            end
-        end
-        self.data.scale = new_scale
-    end
-
-
-
-    for k, v in ipairs(self.strings) do
-        v.W_offset = 0.5 * (self.data.W - v.W)
-        v.H_offset = 0.5 * (self.data.H - v.H + (self.data.offset_y or 0))
     end
     return self.data
 end
