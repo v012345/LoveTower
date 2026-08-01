0dc6fe6b2da4c67a25cdb3b945bfc636b2cfef00
v138468 <v138468@gmail.com>
2026-08-01 12:49:57 +0800

    init

--- api/text_api.lua
+++ api/text_api.lua
@@ -1,11 +1,11 @@
 ---@meta
 
 ---@class (partial) DynaText:Moveable
----@field config TextConfig
+---@field config DynaTextConfig
 
 ---@class DynaTextConfig
 ---@field private data DynaTextConfigData
----@field strings DynaTextString[]
+---@field strings StringConfig[]
 ---@field x_offset number
 ---@field y_offset number
 ---@field pop_delay number
@@ -14,54 +14,6 @@
 ---@field spacing number 字符间距
 ---@field random_element boolean 这个完全就是为了`错印小丑`准备的
 
-
-
-
----@class DynaTextConfigString
----@field ref_table table 引用表
----@field ref_value string 引用值
----@field prefix string
----@field suffix string
----@field scale number 此字符串的自己的缩放比例
----@field outer_colour Color
----@field colour Color
----@field string string 如果 ref_table[ref_value] 不存在, 则使用此 string
-
-
----@class FontConfig
----@field file string
----@field render_scale number
----@field TEXT_HEIGHT_SCALE number
----@field TEXT_OFFSET Vec2
----@field FONTSCALE number
----@field squish number
----@field DESCSCALE number
----@field FONT love.Font
-
-
----@class DynaTextString
----@field letters DynaTextLetter[]
----@field string string
----@field W_offset number 让字符串在水平方向上居中的偏移量
----@field H_offset number 让字符串在垂直方向上居中的偏移量
----@field W number letters 中的字符串在经过 update_text 后, 计算出的以 Tile 为单位的字符串的宽度
----@field H number letters 中的字符串在经过 update_text 后, 计算出的以 Tile 为单位的字符串的高度
-
-
----@class DynaTextLetter
----@field letter love.Text
----@field char string
----@field scale number
----@field offset Vec2
----@field dims Vec2
----@field pop_in number
----@field prefix Color|nil 如果当前字符是前缀字符, 则此颜色会应用到当前字符
----@field suffix Color|nil 如果当前字符是后缀字符, 则此颜色会应用到当前字符
----@field colour Color|nil
----@field r number
-
----@param config? DynaTextConfigData
-function DynaText(config) end
 
 ---可以理解为一个状态机
 ---@class DynaTextConfigData
@@ -108,6 +60,47 @@ function DynaText(config) end
 ---@field colour? Color
 ---@field ref_table table
 ---@field ref_value any
+
+
+
+---@class LetterConfig
+---@field letter love.Text
+---@field char string
+---@field scale number
+---@field offset Vec2
+---@field dims Vec2
+---@field pop_in number
+---@field prefix Color|nil 如果当前字符是前缀字符, 则此颜色会应用到当前字符
+---@field suffix Color|nil 如果当前字符是后缀字符, 则此颜色会应用到当前字符
+---@field colour Color|nil
+---@field r number
+
+
+---@class LetterConfigData
+---@field scale number
+---@field spacing number
+---@field font FontConfig
+---@field colour Color
+
+
+---@class FontConfig
+---@field file string
+---@field render_scale number
+---@field TEXT_HEIGHT_SCALE number
+---@field TEXT_OFFSET Vec2
+---@field FONTSCALE number
+---@field squish number
+---@field DESCSCALE number
+---@field FONT love.Font
+
+
+---comment
+---@param config LetterConfigData
+---@return LetterConfig
+function LetterConfig(config) end
+
+---@param config? DynaTextConfigData
+function DynaText(config) end
 
 ---comment
 ---@param config DynaTextConfigData
