84a7add33165033806d51742805749a0409db6a3
v138468 <v138468@gmail.com>
2026-08-03 21:41:03 +0800

    init

--- asset/scripts/engine/text/init.lua
+++ asset/scripts/engine/text/init.lua
@@ -1,4 +1,4 @@
 require "asset.scripts.engine.text.string_config"
 require "asset.scripts.engine.text.dyna_text_config"
-require "asset.scripts.engine.text.text_config"
+require "asset.scripts.engine.text.letter_config"
 require "asset.scripts.engine.text.dyna_text"
