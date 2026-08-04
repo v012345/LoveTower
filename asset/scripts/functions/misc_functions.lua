4acc6f6836dc224d8d558fae74d8fc9a00ad262e
v138468 <v138468@gmail.com>
2026-08-04 20:36:32 +0800

    init

--- asset/scripts/functions/misc_functions.lua
+++ asset/scripts/functions/misc_functions.lua
@@ -168,7 +168,7 @@ end
 ---@param o any
 ---@return string
 function dump(o)
-    if type(o) == 'table' then
+    if type(o) == 'table' then -- 这里需要考虑元表的情况
         local s = '{ '
         for k, v in pairs(o) do
             if type(k) ~= 'number' then k = '"' .. k .. '"' end
