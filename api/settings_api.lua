e35014d35af4125435df7854cbc23e3442af6528
v138468 <v138468@gmail.com>
2026-08-05 22:26:23 +0800

    init

--- api/settings_api.lua
+++ api/settings_api.lua
@@ -1,6 +1,7 @@
 ---@class (partial) Settings:Object
 ---@field WINDOW WindowSetting 窗口设置
 ---@field QUEUED_CHANGE QueuedChange 队列改变
+---@field DISPLAYS DisplaySetting[] 显示器设置
 
 
 ---@class WindowSetting
@@ -13,3 +14,7 @@
 ---@class QueuedChange
 ---@field screenmode? "Windowed" | "Fullscreen" | "Borderless" 窗口模式
 ---@field selected_display? number 选择的显示器
+
+
+---@class DisplaySetting
+---@field screen_res Size 屏幕分辨率
