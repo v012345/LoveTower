dc8d0125ae7ddd3daa02074e1092ea2b1f002b0c
v138468 <v138468@gmail.com>
2026-08-05 12:45:42 +0800

    init

--- asset/scripts/libs/string_packer.lua
+++ asset/scripts/libs/string_packer.lua
@@ -20,34 +20,34 @@ SOFTWARE.
 
 --I modified this A LOT. Needed to make it quicker if it is being saved to file every few seconds during a game
 function STR_PACK(data, recursive)
-	local ret_str = (recursive and "" or "return ").."{"
-	
-      for i, v in pairs(data) do
-		local type_i, type_v = type(i), type(v)
+    local ret_str = (recursive and "" or "return ") .. "{"
+
+    for i, v in pairs(data) do
+        local type_i, type_v = type(i), type(v)
         assert((type_i ~= "table"), "Data table cannot have an table as a key reference")
         if type_i == "string" then
-			i = '['..string.format("%q",i)..']'
+            i = '[' .. string.format("%q", i) .. ']'
         else
-          	i = "["..i.."]"
+            i = "[" .. i .. "]"
         end
         if type_v == "table" then
-			if v.is and v:is(Object) then
-				v = [["]].."MANUAL_REPLACE"..[["]]
-			else
-				v = STR_PACK(v, true)
-			end
+            if v.is and v:is(Object) then
+                v = [["]] .. "MANUAL_REPLACE" .. [["]]
+            else
+                v = STR_PACK(v, true)
+            end
         else
-          if type_v == "string" then v = string.format("%q", v) end
-		  if type_v == "boolean" then v = v and "true" or "false" end
+            if type_v == "string" then v = string.format("%q", v) end
+            if type_v == "boolean" then v = v and "true" or "false" end
         end
-		ret_str = ret_str..i.."="..v..","
-      end
+        ret_str = ret_str .. i .. "=" .. v .. ","
+    end
 
-	  return ret_str.."}"
+    return ret_str .. "}"
 end
 
 function STR_UNPACK(str)
-  return assert(loadstring(str))()
+    return assert(loadstring(str))()
 end
 
 function get_compressed(_file)
@@ -55,7 +55,7 @@ function get_compressed(_file)
     if file_data ~= nil then
         local file_string = love.filesystem.read(_file)
         if file_string ~= '' then
-            if string.sub(file_string, 1, 6) ~= 'return' then 
+            if string.sub(file_string, 1, 6) ~= 'return' then
                 local success = nil
                 success, file_string = pcall(love.data.decompress, 'string', 'deflate', file_string)
                 if not success then return nil end
@@ -68,5 +68,5 @@ end
 function compress_and_save(_file, _data)
     local save_string = type(_data) == 'table' and STR_PACK(_data) or _data
     save_string = love.data.compress('string', 'deflate', save_string, 1)
-    love.filesystem.write(_file,save_string)
+    love.filesystem.write(_file, save_string)
 end
