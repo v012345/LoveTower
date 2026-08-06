4266ae33de38d511eb694274ca06b63b6078d546
v138468 <v138468@gmail.com>
2026-08-06 22:41:02 +0800

    init

---
+++ asset/scripts/config/stake_cfg.lua
@@ -0,0 +1,20 @@
+---@class (partial) StakeConfig : Object
+local StakeConfig = Object:extend()
+
+function StakeConfig:init()
+    local stake_rows = TableParser.instance:parse("stake")
+    self.stakes = {}
+    for id, row in pairs(stake_rows) do
+        -- 可写外壳: 读不到的字段回退到只读配置行, 运行时字段写在外壳上, 不污染配置表
+        self.stakes[id] = setmetatable({
+            unlocked = row.unlocked,
+        }, { __index = row })
+    end
+end
+
+function StakeConfig:get_stakes()
+    return self.stakes
+end
+
+---@type StakeConfig
+StakeCfg = StakeConfig()
