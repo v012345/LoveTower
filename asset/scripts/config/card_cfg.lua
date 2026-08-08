6c353329269bb540531e4e5ef8ccd86f37fec727
v138468 <v138468@gmail.com>
2026-08-08 22:15:37 +0800

    init

--- asset/scripts/config/card_cfg.lua
+++ asset/scripts/config/card_cfg.lua
@@ -3,12 +3,19 @@ local CardConfig = Object:extend()
 
 function CardConfig:init()
     local jokers = TableParser.instance:parse("joker")
+    local booster_packs = TableParser.instance:parse("booster_pack")
     self.card_config = TableParser.instance:parse("card_config")
     self.unlock_condition = TableParser.instance:parse("unlock_condition")
-   
+
+    self.booster_packs = {}
+    for id, row in pairs(booster_packs) do
+        self.booster_packs[id] = setmetatable({
+            config = self.card_config[id],
+            discovered = row.discovered,
+        }, { __index = row })
+    end
 
     self.jokers = {}
-    -- self.tags = {}
     for id, row in pairs(jokers) do
         -- 可写外壳: 读不到的字段回退到只读配置行, 运行时字段写在外壳上, 不污染配置表
         self.jokers[id] = setmetatable({
