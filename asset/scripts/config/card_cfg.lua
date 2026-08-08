9959f70e31230a751b75a307ee5b98829f13b705
v138468 <v138468@gmail.com>
2026-08-08 21:18:48 +0800

    init

--- asset/scripts/config/card_cfg.lua
+++ asset/scripts/config/card_cfg.lua
@@ -2,17 +2,22 @@
 local CardConfig = Object:extend()
 
 function CardConfig:init()
-    -- local tag_rows = TableParser.instance:parse("tag")
-    self.tag_config = TableParser.instance:parse("card_config")
+    local jokers = TableParser.instance:parse("joker")
+    self.card_config = TableParser.instance:parse("card_config")
+    self.unlock_condition = TableParser.instance:parse("unlock_condition")
+   
 
+    self.jokers = {}
     -- self.tags = {}
-    -- for id, row in pairs(tag_rows) do
-    --     -- 可写外壳: 读不到的字段回退到只读配置行, 运行时字段写在外壳上, 不污染配置表
-    --     self.tags[id] = setmetatable({
-    --         config = self.tag_config[id],
-    --         discovered = row.discovered,
-    --     }, { __index = row })
-    -- end
+    for id, row in pairs(jokers) do
+        -- 可写外壳: 读不到的字段回退到只读配置行, 运行时字段写在外壳上, 不污染配置表
+        self.jokers[id] = setmetatable({
+            config = self.card_config[id],
+            unlock_condition = self.unlock_condition[id],
+            discovered = row.discovered,
+            unlocked = row.unlocked,
+        }, { __index = row })
+    end
 end
 
 -- function CardConfig:get_tags()
