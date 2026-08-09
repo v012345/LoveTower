4b5893576b535b65fc40b181cad3387e8fbc4d0a
v138468 <v138468@gmail.com>
2026-08-09 22:14:31 +0800

    init

--- main.lua
+++ main.lua
@@ -1,10 +1,4 @@
 -- require "temp.temp"
-package.cpath = package.cpath .. ";c:/Users/NightOwl/.vscode/extensions/tangzx.emmylua-0.9.40-win32-x64/debugger/emmy/windows/x64/?.dll"
-local dbg = require("emmy_core")
-dbg.tcpListen("localhost", 9966)
-
-love.timer.sleep(0.1) -- 等待调试器连接
-
 
 
 
