9cf0408a8fd3f55b0134eba85eea91c10dbd3e0b
v138468 <v138468@gmail.com>
2026-08-14 14:00:20 +0800

    init

---
+++ asset/scripts/game/http_manager.lua
@@ -0,0 +1,14 @@
+---@class (partial) HttpManager:Object
+local HttpManager = Object:extend()
+
+function HttpManager:init()
+
+end
+
+---启动存档管理器
+---@param cb? function 进度回调
+function HttpManager:boot(cb)
+
+end
+
+return HttpManager
