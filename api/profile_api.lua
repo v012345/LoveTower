52dabcf980b92764214a8a2dd5804c072ef8492d
v138468 <v138468@gmail.com>
2026-08-14 11:20:09 +0800

    init

---
+++ api/profile_api.lua
@@ -0,0 +1,14 @@
+---@class ProfileInfo
+---@field MEMORY {deck: string, stake: number} 记忆, 用于存储玩家的游戏数据
+---@field stake number 当前赌注
+---@field high_scores {hand: {label: string, amt: number}, furthest_round: {label: string, amt: number}, furthest_ante: {label: string, amt: number}, most_money: {label: string, amt: number}, boss_streak: {label: string, amt: number}, collection: {label: string, amt: number, tot: number}, win_streak: {label: string, amt: number}, current_streak: {label: string, amt: number}, poker_hand: {label: string, amt: number}} 最高得分
+---@field career_stats {c_round_interest_cap_streak: number, c_dollars_earned: number, c_shop_dollars_spent: number, c_tarots_bought: number, c_planets_bought: number, c_playing_cards_bought: number, c_vouchers_bought: number, c_tarot_reading_used: number, c_planetarium_used: number, c_shop_rerolls: number, c_cards_played: number, c_cards_discarded: number, c_losses: number, c_wins: number, c_rounds: number, c_hands_played: number, c_face_cards_played: number, c_jokers_sold: number, c_cards_sold: number, c_single_hand_round_streak: number} 职业统计
+---@field progress {joker_stickers: {tally: number, of: number}, deck_stakes: {tally: number, of: number}, challenges: {tally: number, of: number}} 进度
+---@field joker_usage {tally: number, of: number} 使用次数
+---@field consumeable_usage {tally: number, of: number} 消耗品使用次数
+---@field voucher_usage {tally: number, of: number} 优惠券使用次数
+---@field hand_usage {tally: number, of: number} 手牌使用次数
+---@field deck_usage {tally: number, of: number} 牌组使用次数
+---@field deck_stakes {tally: number, of: number} 牌组赌注
+---@field challenges_unlocked nil 挑战解锁
+---@field challenge_progress {completed: {label: string, amt: number}[], unlocked: {label: string, amt: number}[]} 挑战进度
---
+++ asset/scripts/game/profile.lua
@@ -0,0 +1,10 @@
+---玩家成就记录
+---@class (partial) Profile: Object
+local Profile = Object:extend()
+
+
+function Profile:init()
+
+end
+
+return Profile
