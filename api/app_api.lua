679e6bf71b064f64882b2e2ce31c0d2e2a779a82
v138468 <v138468@gmail.com>
2026-08-14 15:35:21 +0800

    init

--- api/app_api.lua
+++ api/app_api.lua
@@ -27,4 +27,4 @@
 ---@field SAVE_MANAGER SaveManager 存档管理器
 ---@field HTTP_MANAGER HttpManager 网络管理器
 ---@field SHADERS table<string, Shader> 所有着色器
----@field LANG any 当前使用的语言对象
+---@field LANG LanguageConfigItem 当前使用的语言对象
