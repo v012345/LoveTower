6bebf88a0af93a09c096baf623d526921f5d64c5
v138468 <v138468@gmail.com>
2026-08-14 17:40:22 +0800

    init

--- asset/scripts/config/atlas_cfg.lua
+++ asset/scripts/config/atlas_cfg.lua
@@ -1,4 +1,15 @@
+---@class AtlasConfigItem
+---@field Id string
+---@field name string
+---@field path string[]
+---@field dpiscale number[]
+---@field is_animation boolean
+---@field py number
+---@field px number
+---@field frames number
+
 ---@class (partial) AtlasConfig : Object
+---@field cfg table<string, AtlasConfigItem>
 local AtlasConfig = Object:extend()
 
 function AtlasConfig:init()
