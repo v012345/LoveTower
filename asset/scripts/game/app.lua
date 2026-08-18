25c3915790de426eaf7ad6902fb5a955ec0da5dd
v138468 <v138468@gmail.com>
2026-08-18 13:45:45 +0800

    init

--- asset/scripts/game/app.lua
+++ asset/scripts/game/app.lua
@@ -519,37 +519,13 @@ function App:draw()
         local fps = love.timer.getFPS()
         love.graphics.print("Current FPS: " .. fps, 10, 10)
 
-        if self.check and self.SETTINGS.data.perf_mode then
-            local section_h = 30
-            local resolution = 60 * section_h
-            local poll_w = 1
-            local v_off = 100
-            for a, b in ipairs({ self.check.update, self.check.draw }) do
-                for k, v in ipairs(b.checkpoint_list) do
-                    love.graphics.setColor(0, 0, 0, 0.2)
-                    love.graphics.rectangle('fill', 12, 20 + v_off, poll_w + poll_w * #v.trend, -section_h + 5)
-                    for kk, vv in ipairs(v.trend) do
-                        if a == 2 then
-                            love.graphics.setColor(0.3, 0.7, 0.7, 1)
-                        else
-                            love.graphics.setColor(self:state_col(v.states[kk] or 123))
-                        end
-                        love.graphics.rectangle('fill', 10 + poll_w * kk, 20 + v_off, 5 * poll_w, -(vv) * resolution)
-                    end
-                    love.graphics.setColor(a == 2 and 0.5 or 1, a == 2 and 1 or 0.5, 1, 1)
-                    love.graphics.print(v.label .. ': ' .. (string.format("%.2f", 1000 * (v.average or 0))) .. '\n', 10, -section_h + 30 + v_off)
-                    v_off = v_off + section_h
-                end
-            end
+        if self.SETTINGS.data.perf_mode then
+            self.Performance:draw()
         end
 
         love.graphics.pop()
     end
     self.Performance:timer_checkpoint('debug', 'draw')
-end
-
-function App:state_col(_state)
-    return (_state * 15251252.2 / 5.132) % 1, (_state * 1422.5641311 / 5.42) % 1, (_state * 1522.1523122 / 5.132) % 1, 1
 end
 
 function App:set_render_settings()
--- asset/scripts/game/performance.lua
+++ asset/scripts/game/performance.lua
@@ -53,4 +53,34 @@ function Performance:timer_checkpoint(label, type, reset)
     end
 end
 
+function Performance:state_col(_state)
+    return (_state * 15251252.2 / 5.132) % 1, (_state * 1422.5641311 / 5.42) % 1, (_state * 1522.1523122 / 5.132) % 1, 1
+end
+
+function Performance:draw()
+    love.graphics.push()
+    local section_h = 30
+    local resolution = 60 * section_h
+    local poll_w = 1
+    local v_off = 100
+    for a, b in ipairs({ self.check.update, self.check.draw }) do
+        for k, v in ipairs(b.checkpoint_list) do
+            love.graphics.setColor(0, 0, 0, 0.2)
+            love.graphics.rectangle('fill', 12, 20 + v_off, poll_w + poll_w * #v.trend, -section_h + 5)
+            for kk, vv in ipairs(v.trend) do
+                if a == 2 then
+                    love.graphics.setColor(0.3, 0.7, 0.7, 1)
+                else
+                    love.graphics.setColor(self:state_col(v.states[kk] or 123))
+                end
+                love.graphics.rectangle('fill', 10 + poll_w * kk, 20 + v_off, 5 * poll_w, -(vv) * resolution)
+            end
+            love.graphics.setColor(a == 2 and 0.5 or 1, a == 2 and 1 or 0.5, 1, 1)
+            love.graphics.print(v.label .. ': ' .. (string.format("%.2f", 1000 * (v.average or 0))) .. '\n', 10, -section_h + 30 + v_off)
+            v_off = v_off + section_h
+        end
+    end
+    love.graphics.pop()
+end
+
 return Performance
