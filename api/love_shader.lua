ab8759ad411577ce3eb04a278f4b6aaea1f06bcb
v138468 <v138468@gmail.com>
2026-08-18 11:08:29 +0800

    init

---
+++ api/love_shader.lua
@@ -0,0 +1,15 @@
+---@meta
+
+---@class Shader: love.Object
+Shader = {}
+
+---Sends one or more values to a special (uniform) variable inside the shader. Uniform variables have to be marked using the uniform or extern keyword, e.g
+---@param name string Name of the number to send to the shader.
+---@param number number Number to send to store in the uniform variable.
+---@param ... unknown Additional numbers to send if the uniform variable is an array.
+function Shader:send(name, number, ...) end
+
+---@param name string Name of the vector to send to the shader.
+---@param vector table Numbers to send to the uniform variable as a vector. The number of elements in the table determines the type of the vector (e.g. two numbers -> vec2). At least two and at most four numbers can be used.
+---@param ... table Additional vectors to send if the uniform variable is an array. All vectors need to be of the same size (e.g. only vec3's).
+function Shader:send(name, vector, ...) end
