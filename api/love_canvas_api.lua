20cd1d02e13dcc49c09c0dbbb713e731125c79d3
v138468 <v138468@gmail.com>
2026-08-18 11:00:16 +0800

    init

--- api/love_canvas_api.lua
+++ api/love_canvas_api.lua
@@ -1,8 +1,9 @@
 ---@meta
 
----@class Canvas: Drawable
+---@class Canvas: Texture
 Canvas = {}
 ---@param filter "nearest" | "linear"
 ---@param filtermag "nearest" | "linear"
 ---@return nil
 function Canvas:setFilter(filter, filtermag) end
+
---
+++ api/love_texture_api.lua
@@ -0,0 +1,8 @@
+---@meta
+
+---@class Texture:Drawable
+Texture = {}
+
+---Gets the height in pixels of the Texture.
+---@return number pixelheight The height of the Texture, in pixels.
+function Texture:getPixelHeight() end
