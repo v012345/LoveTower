93cb333be4997157d33bf239cd3b86aee7c0127e
v138468 <v138468@gmail.com>
2026-08-18 18:08:10 +0800

    init

--- asset/scripts/game/file_handler.lua
+++ asset/scripts/game/file_handler.lua
@@ -10,6 +10,21 @@ function FileHandler:init(app)
     self.last_sent_stage = STAGES.NONE
     self.last_sent_pause = false
     self.run = false
+    self.last_sent_time = 0
+end
+
+function FileHandler:is_need_save()
+    if not self.update_queued then return false end
+
+    if self.force then return true end
+
+    if self.last_sent_stage ~= self.app.STAGE then return true end
+
+    if (self.last_sent_pause ~= self.app.SETTINGS:is_paused()) and self.run then return true end
+
+    if self.last_sent_time < (self.app.TIMERS.UPTIME - self.app.Features:get_save_timer()) then return true end
+
+    return false
 end
 
 return FileHandler
