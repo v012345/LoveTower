52253e1c12eca3fbd3aaf1c571fdb2adbd7dd55e
NightOwl <v138468@gmail.com>
2026-08-20 23:36:23 -0700

    init

--- asset/scripts/game/card.lua
+++ asset/scripts/game/card.lua
@@ -17,6 +17,7 @@ function Card:init(T, card, center, params, container)
         card = card or {},
         center = center
     }
+    self:set_ability(center, true)
     self.sprite_facing = 'front'
 
     if getmetatable(self) == Card then
@@ -24,10 +25,24 @@ function Card:init(T, card, center, params, container)
     end
 end
 
+---设置卡牌的能力
 function Card:set_ability(center, initial, delay_sprites)
+    if delay_sprites then
+        App.E_MANAGER:add_event(Event({
+            func = function()
+                if not self.REMOVED then
+                    self:set_sprites(center)
+                end
+                return true
+            end
+        }))
+    else
+        self:set_sprites(center)
+    end
 end
 
 function Card:set_sprites(_center, _front)
+    print(_center)
 end
 
 ---@param layer 'both' | 'shadow' | 'card'
