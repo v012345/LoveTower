f09a59b178d3efe2f7f7a711c3542da1be857872
NightOwl <v138468@gmail.com>
2026-08-23 08:57:49 -0700

    init

--- api/love_joystick_api.lua
+++ api/love_joystick_api.lua
@@ -2,3 +2,7 @@
 
 ---@class love.joystick : love.object
 love.joystick = love.joystick or {}
+
+---Gets a list of connected Joysticks.
+---@return love.joystick[] joysticks The list of currently connected Joysticks.
+function love.joystick.getJoysticks() end
