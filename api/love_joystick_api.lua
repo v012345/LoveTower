3bd98f475e662754dc45a2580b762a3d7b9cd311
NightOwl <v138468@gmail.com>
2026-08-23 09:32:23 -0700

    init

--- api/love_joystick_api.lua
+++ api/love_joystick_api.lua
@@ -4,11 +4,11 @@
 love.joystick = love.joystick or {}
 
 ---Gets a list of connected Joysticks.
----@return love.joystick[] joysticks The list of currently connected Joysticks.
+---@return Joystick[] joysticks The list of currently connected Joysticks.
 function love.joystick.getJoysticks() end
 
 ---@class Joystick
+Joystick = Joystick or {}
 
-function love.joystick:getGamepadMappingString()
-
-end
+---@return string mappingstring `(nil)` A string containing the Joystick's gamepad mappings, or nil if the Joystick is not recognized as a gamepad.
+function Joystick:getGamepadMappingString() end
