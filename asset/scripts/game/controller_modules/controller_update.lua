5a7b21f1cbc0d3fc166d7a050392921fb3caab9a
NightOwl <v138468@gmail.com>
2026-08-24 04:40:15 -0700

    init

--- asset/scripts/game/controller_modules/controller_update.lua
+++ asset/scripts/game/controller_modules/controller_update.lua
@@ -1,6 +1,12 @@
 ---@class (partial) Controller : GameObject
 
 function Controller:update(dt)
-    self.locked = false
     self.locks.wipe = not not App.screenwipe
+    self.locked = false
+    for k, v in pairs(self.locks) do
+        if v then
+            self.locked = true
+            break
+        end
+    end
 end
